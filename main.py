b8b04d9e87fdf4ca45e1a20fe008e53588cf09d3
dnk753 <77626206+dnk753@users.noreply.github.com>
2026-02-07 02:05:27 +0300

    Update bot token and add image command
    Replaced the bot token with a hardcoded value and added a new command for image handling.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder  # новый импор
 from variables import *
 import os
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = '7884118333:AAHeAFG6m16Fedht9HPxd6vMcb8urj_8qBc' #os.getenv('BOT_TOKEN')
 
 
 bot = Bot(token=TOKEN)
@@ -25,6 +25,10 @@ async def cmd_start(message: Message):
 async def cmd_release(message: Message):
     await message.answer(https_release)
 
+@dp.message(Command('img'))
+async def cmd_img(message: Message):
+    image = FSInputFile(img)
+    await message.answer_photo(image, caption = '=)')
 
 async def main():
     await dp.start_polling(bot)
